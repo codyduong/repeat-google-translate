8597ab8a756441e33d795e02acaf49d927b1aff2
codyduong <duongcody507@gmail.com>
2021-04-03 00:10:44 -0500

    Cleanup

---
+++ src/LanguageForm.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+import SetLanguage from "./SetLanguage"
+
+const LanguageField = () => {
+  const [input, setInput] = useState()
+  const [output, setOutput] = useState()
+
+  return (
+    <div>
+      <SetLanguage 
+        
+      />
+    </div>
+  )
+}
+
+export default LanguageField
